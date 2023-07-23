1fdb1c4bb1a42260bdc0f74c169eba702e97f12f
deus-tll <millerartemka14@gmail.com>
2023-07-23 23:07:10 +0300

    update

--- Homework_005/Library/Pages/Index.cshtml.cs
+++ Homework_005/Library/Pages/Index.cshtml.cs
@@ -12,33 +12,25 @@ namespace Library.Pages
         private const string WWWWROOT = "wwwroot";
         private const string DATA_FOLDER = "data";
         private const string DATA_FETCHED_FILE = "fetchedData.json";
-        private readonly IConfiguration _configuration;
-        private readonly JsonSerializerOptions _jsonSerializerOptions;
         private readonly string _publicKey;
         private readonly string _privateKey;
+        private readonly string _lastFetchDate;
         private readonly int _limit = 0;
         public required List<Comic>? Comics { get; set; }
         public IndexModel(IConfiguration configuration)
         {
-            _configuration = (IConfigurationRoot)configuration;
-
             _publicKey = configuration["ApiInfo:PublicKey"] ?? "";
             _privateKey = configuration["ApiInfo:PrivateKey"] ?? "";
+            _lastFetchDate = configuration["ApiInfo:LastFetchDate"] ?? "";
             _ = int.TryParse(configuration["ApiInfo:Limit"], out _limit);
-
-            _jsonSerializerOptions = new JsonSerializerOptions
-            {
-                WriteIndented = true
-            };
         }
 
 
         public async Task OnGet()
         {
-            string? lastFetchDate = GetLastFetchDateFromAppSettings();
             DateTime now = DateTime.Now;
 
-            if (string.IsNullOrEmpty(lastFetchDate) || (now - DateTime.Parse(lastFetchDate)).TotalDays >= 1)
+            if (string.IsNullOrEmpty(_lastFetchDate) || (now - DateTime.Parse(_lastFetchDate)).TotalDays >= 1)
             {
                 var result = await GetComics();
 
@@ -72,16 +64,6 @@ namespace Library.Pages
             }
         }
 
-        private static dynamic? DeserializeDynamic(string json)
-        {
-            return JsonConvert.DeserializeObject<dynamic>(json);
-        }
-
-        private static string SerializeDynamic(dynamic obj)
-        {
-            return JsonConvert.SerializeObject(obj, Formatting.Indented);
-        }
-
 
         public async Task<List<Comic>?> GetComics()
         {
@@ -90,17 +72,6 @@ namespace Library.Pages
             string apiUrl = $"http://gateway.marvel.com/v1/public/comics?ts={timeStamp}&apikey={_publicKey}&hash={hash}&limit={IsLimit()}";
 
             return await ApiRequestHelper.GetComics(apiUrl);
-        }
-
-
-        private static string? GetLastFetchDateFromAppSettings()
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            return configuration["ApiInfo:LastFetchDate"];
         }
 
 
@@ -113,6 +84,18 @@ namespace Library.Pages
 
             var filePath = Path.Combine(WWWWROOT, DATA_FOLDER, DATA_FETCHED_FILE);
             await System.IO.File.WriteAllTextAsync(filePath, jsonData);
+        }
+
+
+        private static dynamic? DeserializeDynamic(string json)
+        {
+            return JsonConvert.DeserializeObject<dynamic>(json);
+        }
+
+
+        private static string SerializeDynamic(dynamic obj)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented);
         }
 
 
