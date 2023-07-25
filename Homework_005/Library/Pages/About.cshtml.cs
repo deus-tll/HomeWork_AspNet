1599c5497988afee1e8c775c9786841b9fef7953
deus-tll <107714440+deus-tll@users.noreply.github.com>
2023-07-25 18:20:26 +0300

    Update About.cshtml.cs

--- Homework_005/Library/Pages/About.cshtml.cs
+++ Homework_005/Library/Pages/About.cshtml.cs
@@ -2,7 +2,6 @@ using Library.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-//я знаю, що я тут один момент упустив, я дороблю
 namespace Library.Pages
 {
     public class AboutModel : PageModel
