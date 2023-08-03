644acc1a62e7694f25422df06435f16146911d4a
deus-tll <millerartemka14@gmail.com>
2023-08-03 18:41:39 +0300

    task_1 commit

---
+++ Homework_009/Task_1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_1.Models;
+
+namespace Task_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task Index()
+        {
+            Response.ContentType = "text/html;charset=utf-8";
+
+            CurrentMachineOS currentMachineOS = new();
+
+            await Response.WriteAsync($"<h2>NAME OF CURRENT SERVER'S OS: {currentMachineOS.OS}</h2>" +
+                $"<h2><strong>VERSION OF CURRENT SERVER'S OS: </strong>{currentMachineOS.OS_Version}</h2>");
+        }
+    }
+}
---
+++ Homework_009/Task_1/Models/CurrentMachineOC.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_1.Models
+{
+    public class CurrentMachineOS
+    {
+        public string OS { get; private set; }
+        public string OS_Version { get; private set; }
+
+        public CurrentMachineOS()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            Version version = os.Version;
+
+            string osName = os.Platform switch
+            {
+                PlatformID.Win32NT => "Windows",
+                PlatformID.Unix => "Unix",
+                PlatformID.MacOSX => "MacOS",
+                _ => "Unknown"
+            };
+
+            OS = osName;
+            OS_Version = version.ToString();
+        }
+    }
+}
