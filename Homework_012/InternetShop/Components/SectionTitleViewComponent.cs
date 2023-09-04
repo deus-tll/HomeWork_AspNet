e3731b8f5e1fdd9b06c1722b46bad04d48ff459f
deus-tll <millerartemka14@gmail.com>
2023-09-05 00:39:59 +0300

    homework_014 finished

---
+++ Homework_012/InternetShop/Components/SectionTitleViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternetShop.Components
+{
+    public class SectionTitleViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            if (ViewData["Title"] is string title && !string.IsNullOrEmpty(title))
+            {
+                return View("Default", title);
+            }
+
+            return View("Default", "Заголовок відсутній");
+        }
+    }
+}
